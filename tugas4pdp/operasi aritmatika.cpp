b9cfb6794c56e2e345b206e2b8e599eceed8b688
Kynnn <87553957+Spectrix@users.noreply.github.com>
2025-09-22 08:37:24 +0700

    Add files via upload

---
+++ tugas4pdp/operasi aritmatika.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int bil1 = 70;
+	int bil2 = 49;
+
+	cout << "Penjumlahan: " << (bil1 + bil2) << endl;
+	cout << "Pengurangan: " << (bil1 - bil2) << endl;
+	cout << "Perkalian: " << (bil1 * bil2) << endl;
+	cout << "Pembagian: " << (bil1 / bil2) << endl;
+	cout << "Modulus: " << (bil1 % bil2) << endl;
+
+	return 0;
+}
---
+++ tugas4pdp/tugas4.0.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string name = "Muhammad Ilham";
+    cout << name << endl;
+    return 0;
+}
