b9ad8112563e200792d80906ddbf162baf8d7878
Kynnn <87553957+Spectrix@users.noreply.github.com>
2025-09-22 09:07:36 +0700

    Add files via upload

--- tugas4pdp/operasi aritmatika.cpp
+++ tugas4pdp/operasi aritmatika.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
 	int bil1 = 70;
 	int bil2 = 49;
 
-	cout << "Penjumlahan: " << (bil1 + bil2) << endl;
-	cout << "Pengurangan: " << (bil1 - bil2) << endl;
-	cout << "Perkalian: " << (bil1 * bil2) << endl;
-	cout << "Pembagian: " << (bil1 / bil2) << endl;
-	cout << "Modulus: " << (bil1 % bil2) << endl;
+	string operasi;
+
+	operasi = "Penjumlahan";
+	cout << operasi << ": " << (bil1 + bil2) << endl;
+
+	operasi = "Pengurangan";
+	cout << operasi << ": " << (bil1 - bil2) << endl;
+
+	operasi = "Perkalian";
+	cout << operasi << ": " << (bil1 * bil2) << endl;
+
+	operasi = "Pembagian";
+	cout << operasi << ": " << (bil1 / bil2) << endl;
+
+	operasi = "Modulus";
+	cout << operasi << ": " << (bil1 % bil2) << endl;
 
 	return 0;
 }
---
+++ tugas4pdp/operator perbandingan.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x = 10;
+	int y = 6;
+	int umurA = 32;
+	int umur8 = 32;
+
+	cout << "x == y: " << (x == y) << endl;
+	cout << "x != y: " << (x != y) << endl;
+	cout << "x > y: " << (x > y) << endl;
+	cout << "x < y: " << (x < y) << endl;
+	cout << "x >= y: " << (x >= y) << endl;
+	cout << "x <= y: " << (x <= y) << endl;
+
+	cout << "umurA == umur8: " << (umurA == umur8) << endl;
+	cout << "umurA != umur8: " << (umurA != umur8) << endl;
+	cout << "umurA > umur8: " << (umurA > umur8) << endl;
+	cout << "umurA < umur8: " << (umurA < umur8) << endl;
+	cout << "umurA >= umur8: " << (umurA >= umur8) << endl;
+	cout << "umurA <= umur8: " << (umurA <= umur8) << endl;
+
+	return 0;
+}
+ 
